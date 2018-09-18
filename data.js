3af94b7a4505d958e2e7399de70a752eb1640717
Marco Schmalz <marco@schess.ch>
2018-09-18 17:15:33 +0200

    Beispiel für einen Fragenkatalog

---
+++ data.js
@@ -0,0 +1,34 @@
+fragenKatalog = {
+  frage1: {
+    frage: 'Wie ist das Wetter?',
+    antworten: [
+      {antwort: 'Schön', next: 'frage2'},
+      {antwort: 'Bedeckt', next: 'frage3'},
+      {antwort: 'Regnerisch', next: 'frage4'},
+    ],
+  },
+  frage2: {
+    frage: 'Was tun bei diesem schönen Wetter?',
+    antworten: [
+      {antwort: 'Gamen', pic: 'https://techtalk.currys.co.uk/media/683769/Fotolia_73249583_M%20600.jpg'},
+      {antwort: 'Fernsehen', pic: 'https://www.postmedien.ch/wp-content/uploads/2015/09/kindertv.jpg'},
+      {antwort: 'Aufräumen', pic: 'https://www.toonpool.com/user/362/files/aufraeumen_990935.jpg'},
+    ],
+  },
+  frage3: {
+    frage: 'Was tun bei diesem naja, nicht gerade wahnsinnigen Wetter?',
+    antworten: [
+      {antwort: 'Sünnele', pic: 'https://www.tripsavvy.com/thmb/kx2QZntlqrA6_rCU6ezLu8ewARs=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/Dameisha-Beach-5989134d396e5a00114810dd.jpg'},
+      {antwort: 'Grassskifahren', pic: 'http://skiclub-winterthur.ch/sites/default/files/3278.jpg'},
+      {antwort: 'Ballon fahren', pic: 'https://www.dvfg.de/fileadmin/user_upload/content/Heissluftballons.jpg'},
+    ],
+  },
+  frage4: {
+    frage: 'Was tun bei diesem doofen Wetter?',
+    antworten: [
+      {antwort: 'Schlammschlacht', pic: 'https://weltexpress.info/wp-content/uploads/2017/06/Schlamm-Soldat-Schlammschlacht-QF-Pixel-1920-Quelle-Source-Pixabay-gemeinfrei-CC0-Public-Domain.jpg'},
+      {antwort: 'Dancing in the rain', pic: 'https://static1.squarespace.com/static/586ed399bf629a58a34f76fe/t/58aca3afc534a5aed421e374/1487709294826/'},
+      {antwort: 'Velofahren', pic: 'https://tdsportswear.com/wp-content/uploads/2016/01/Cycle-wet1-1-1170x570.jpg'},
+    ],
+  },
+}
