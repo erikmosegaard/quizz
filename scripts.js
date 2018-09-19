3fd94d669948acb9dc285d639acda28d0cfa95a0
holoarcher <josiahksch@gmail.com>
2018-09-20 00:00:47 +0200

    fixed alot of stuff TODO: cleanup and scaling

---
+++ scripts.js
@@ -0,0 +1,31 @@
+$(".circle_center").click(function () {
+    $(this).css("transform", "rotate(180deg) scale(0.1)");
+   
+    
+    $(".circle_center").fadeOut(1500);
+    $("#buffer_top").css("background-color", "transparent")
+    $("#bottom_element").css("border", "none")
+    $("#bottom_element").css("background-color", "transparent")
+    $("#antwortenwrap").fadeIn(2500);
+    $(".frage").fadeIn(2500);
+    $(".antworten").fadeIn(3500);
+});
+
+/* $("#antworteins").click(function (){
+    $("#antwortzwei").fadeOut(1000);
+    $("#antwortdrei").fadeOut(1000);
+    $("#antworteins").fadeOut(1000);
+    $(".frage").fadeOut(500);
+    $(".fragezwei").fadeIn(1000);
+    $(".antwortenzwei").fadeIn(1000);
+})
+
+$("#antwortzwei").click(function (){
+    $("#antworteins").fadeOut(1000);
+    $("#antwortdrei").fadeOut(1000);
+})
+
+$("#antwortdrei").click(function (){
+    $("#antworteins").fadeOut(1000);
+    $("#antwortzwei").fadeOut(1000);
+}) */
