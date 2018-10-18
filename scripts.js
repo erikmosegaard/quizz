75d7c3082731ed2e6f70175f94abc08b84021b3d
Marco Schmalz <marco@schess.ch>
2018-10-18 16:07:07 +0200

    Intro video

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
-$(".circle_center").click(function () {
-    $(this).css("transform", "rotate(180deg) scale(0.1)");
-   
-    
-    $(".circle_center").fadeOut(1500);
-    $("#buffer_top").css("background-color", "transparent")
-    $("#bottom_element").css("border", "none")
-    $("#bottom_element").css("background-color", "transparent")
-    $("#antwortenwrap").fadeIn(2500);
-    $(".frage").fadeIn(2500);
-    $(".antworten").fadeIn(3500);
-});
+// $(".circle_center").click(function () {
+//     $(this).css("transform", "rotate(180deg) scale(0.1)");
+
+
+//     $(".circle_center").fadeOut(1500);
+//     $("#buffer_top").css("background-color", "transparent")
+//     $("#bottom_element").css("border", "none")
+//     $("#bottom_element").css("background-color", "transparent")
+//     $("#antwortenwrap").fadeIn(2500);
+//     $(".frage").fadeIn(2500);
+//     $(".antworten").fadeIn(3500);
+// });
 
 /* $("#antworteins").click(function (){
     $("#antwortzwei").fadeOut(1000);
